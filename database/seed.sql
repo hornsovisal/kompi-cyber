43ec330a74c668262022ae294b7b53d18fdac55d
Vaathanaa007 <vaathanaa007@Vaathanaa-Mac.local>
2026-03-07 10:34:12 +0700

    Update database

--- database/seed.sql
+++ database/seed.sql
@@ -16,148 +16,316 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `certificates`
+-- Table structure for table `certificates`
 --
 
-LOCK TABLES `certificates` WRITE;
-/*!40000 ALTER TABLE `certificates` DISABLE KEYS */;
-/*!40000 ALTER TABLE `certificates` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `certificates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `certificates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` char(36) DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `certificate_code` varchar(100) DEFAULT NULL,
+  `issued_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pdf_path` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `certificate_code` (`certificate_code`),
+  UNIQUE KEY `uniq_user_course_cert` (`user_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `certificates_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `certificates_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `code_submissions`
+-- Table structure for table `code_submissions`
 --
 
-LOCK TABLES `code_submissions` WRITE;
-/*!40000 ALTER TABLE `code_submissions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `code_submissions` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `code_submissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `code_submissions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `exercise_id` int DEFAULT NULL,
+  `user_id` char(36) DEFAULT NULL,
+  `code` text,
+  `stdout` text,
+  `stderr` text,
+  `execution_time_ms` int DEFAULT NULL,
+  `memory_used_mb` int DEFAULT NULL,
+  `passed` tinyint(1) DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_exercise_id` (`exercise_id`),
+  KEY `idx_submitted_at` (`submitted_at`),
+  CONSTRAINT `code_submissions_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
+  CONSTRAINT `code_submissions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `courses`
+-- Table structure for table `courses`
 --
 
-LOCK TABLES `courses` WRITE;
-/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'Intro to SQL','Learn the basics of databases.',1,'Beginner','550e8400-e29b-41d4-a716-446655440000','2026-03-05 03:42:56');
-/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `description` text,
+  `track_id` int DEFAULT NULL,
+  `level` varchar(50) DEFAULT NULL,
+  `created_by` char(36) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `track_id` (`track_id`),
+  KEY `idx_created_by` (`created_by`),
+  CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`track_id`) REFERENCES `tracks` (`id`),
+  CONSTRAINT `courses_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `enrollments`
+-- Table structure for table `enrollments`
 --
 
-LOCK TABLES `enrollments` WRITE;
-/*!40000 ALTER TABLE `enrollments` DISABLE KEYS */;
-INSERT INTO `enrollments` VALUES (3,'550e8400-e29b-41d4-a716-446655440000',1,'2026-03-06 07:15:18');
-/*!40000 ALTER TABLE `enrollments` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `enrollments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enrollments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` char(36) DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `enrolled_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_user_course` (`user_id`,`course_id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_course_id` (`course_id`),
+  CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `exercise_test_cases`
+-- Table structure for table `exercise_test_cases`
 --
 
-LOCK TABLES `exercise_test_cases` WRITE;
-/*!40000 ALTER TABLE `exercise_test_cases` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exercise_test_cases` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `exercise_test_cases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercise_test_cases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `exercise_id` int DEFAULT NULL,
+  `input` text,
+  `expected_output` text,
+  `is_hidden` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exercise_id` (`exercise_id`),
+  CONSTRAINT `exercise_test_cases_ibfk_1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `exercises`
+-- Table structure for table `exercises`
 --
 
-LOCK TABLES `exercises` WRITE;
-/*!40000 ALTER TABLE `exercises` DISABLE KEYS */;
-INSERT INTO `exercises` VALUES (5,1,'Simple SELECT','Write a query to fetch all rows.','SELECT * FROM users;',1,2000,64,'2026-03-06 07:27:25'),(6,1,'Filter with WHERE','Write a query to fetch only students.','SELECT * FROM users WHERE role_id=3;',1,2000,64,'2026-03-06 07:27:25');
-/*!40000 ALTER TABLE `exercises` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `exercises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercises` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lesson_id` int DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL,
+  `instructions_md` text,
+  `starter_code` text,
+  `language_id` int DEFAULT NULL,
+  `time_limit_ms` int DEFAULT NULL,
+  `memory_limit_mb` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `lesson_id` (`lesson_id`),
+  KEY `language_id` (`language_id`),
+  CONSTRAINT `exercises_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`),
+  CONSTRAINT `exercises_ibfk_2` FOREIGN KEY (`language_id`) REFERENCES `tracks` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lesson_progress`
+-- Table structure for table `lesson_progress`
 --
 
-LOCK TABLES `lesson_progress` WRITE;
-/*!40000 ALTER TABLE `lesson_progress` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lesson_progress` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `lesson_progress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lesson_progress` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lesson_id` int DEFAULT NULL,
+  `user_id` char(36) DEFAULT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  `completed_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_user_lesson_progress` (`user_id`,`lesson_id`),
+  KEY `lesson_id` (`lesson_id`),
+  CONSTRAINT `lesson_progress_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`),
+  CONSTRAINT `lesson_progress_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lessons`
+-- Table structure for table `lessons`
 --
 
-LOCK TABLES `lessons` WRITE;
-/*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
-INSERT INTO `lessons` VALUES (1,1,'SELECT Statements','# Welcome to SQL\nIn this lesson you learn...',1,'2026-03-05 03:43:09');
-/*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `lessons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lessons` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `course_id` int DEFAULT NULL,
+  `title` varchar(200) DEFAULT NULL,
+  `content_md` varchar(255) DEFAULT NULL,
+  `lesson_order` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_course_lesson_order` (`course_id`,`lesson_order`),
+  CONSTRAINT `lessons_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `quiz_answers`
+-- Table structure for table `quiz_answers`
 --
 
-LOCK TABLES `quiz_answers` WRITE;
-/*!40000 ALTER TABLE `quiz_answers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quiz_answers` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `quiz_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quiz_answers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `attempt_id` int DEFAULT NULL,
+  `question_id` int DEFAULT NULL,
+  `selected_option_id` int DEFAULT NULL,
+  `is_correct` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `attempt_id` (`attempt_id`),
+  KEY `question_id` (`question_id`),
+  KEY `selected_option_id` (`selected_option_id`),
+  CONSTRAINT `quiz_answers_ibfk_1` FOREIGN KEY (`attempt_id`) REFERENCES `quiz_attempts` (`id`),
+  CONSTRAINT `quiz_answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `quiz_questions` (`id`),
+  CONSTRAINT `quiz_answers_ibfk_3` FOREIGN KEY (`selected_option_id`) REFERENCES `quiz_options` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `quiz_attempts`
+-- Table structure for table `quiz_attempts`
 --
 
-LOCK TABLES `quiz_attempts` WRITE;
-/*!40000 ALTER TABLE `quiz_attempts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quiz_attempts` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `quiz_attempts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quiz_attempts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lesson_id` int DEFAULT NULL,
+  `user_id` char(36) DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  `attempt_no` int DEFAULT NULL,
+  `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_user_lesson` (`user_id`,`lesson_id`),
+  KEY `lesson_id` (`lesson_id`),
+  CONSTRAINT `quiz_attempts_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`),
+  CONSTRAINT `quiz_attempts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `quiz_options`
+-- Table structure for table `quiz_options`
 --
 
-LOCK TABLES `quiz_options` WRITE;
-/*!40000 ALTER TABLE `quiz_options` DISABLE KEYS */;
-INSERT INTO `quiz_options` VALUES (1,1,'GET',0),(2,1,'FETCH',0),(3,1,'SELECT',1);
-/*!40000 ALTER TABLE `quiz_options` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `quiz_options`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quiz_options` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int DEFAULT NULL,
+  `option_text` text,
+  `is_correct` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `quiz_options_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `quiz_questions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `quiz_questions`
+-- Table structure for table `quiz_questions`
 --
 
-LOCK TABLES `quiz_questions` WRITE;
-/*!40000 ALTER TABLE `quiz_questions` DISABLE KEYS */;
-INSERT INTO `quiz_questions` VALUES (1,1,'What keyword is used to fetch data?','2026-03-05 03:43:19');
-/*!40000 ALTER TABLE `quiz_questions` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `quiz_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `quiz_questions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lesson_id` int DEFAULT NULL,
+  `question_text` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `lesson_id` (`lesson_id`),
+  CONSTRAINT `quiz_questions_ibfk_1` FOREIGN KEY (`lesson_id`) REFERENCES `lessons` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `roles`
+-- Table structure for table `roles`
 --
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'admin'),(2,'instructor'),(3,'student');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tracks`
+-- Table structure for table `tracks`
 --
 
-LOCK TABLES `tracks` WRITE;
-/*!40000 ALTER TABLE `tracks` DISABLE KEYS */;
-INSERT INTO `tracks` VALUES (3,'Data Structures'),(1,'Fullstack Web'),(2,'Python Mastery');
-/*!40000 ALTER TABLE `tracks` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `tracks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tracks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Table structure for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('550e8400-e29b-41d4-a716-446655440000','Test Admin','admin@nextgen.com','hashed_pw_here',1,'2026-03-05 03:42:35'),('550e8400-e29b-41d4-a716-446655440001','Jane Instructor','instructor@nextgen.com','hashed_pw_instructor',2,'2026-03-06 07:08:37'),('550e8400-e29b-41d4-a716-446655440002','John Student','student@nextgen.com','hashed_pw_student',3,'2026-03-06 07:08:37');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` char(36) NOT NULL,
+  `full_name` varchar(120) DEFAULT NULL,
+  `email` varchar(150) NOT NULL,
+  `password_hash` text NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -168,4 +336,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-06 23:52:26
+-- Dump completed on 2026-03-07 10:33:17
