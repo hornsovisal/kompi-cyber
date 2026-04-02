d84f8c38ce7a558783e413a3d64d33735e0b37eb
= <=>
2026-04-02 16:31:50 +0700

    fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,13 +7,25 @@ const authMiddleware = require("../middleware/authMiddleware");
 router.post(
   "/register",
   authMiddleware.validateRegister,
-  authController.registerUser,
+  (req, res) => authController.registerUser(req, res),
 );
 
-router.post("/login", authMiddleware.validateLogin, authController.loginUser);
-router.post("/forgot-password", authController.forgotPassword);
-router.post("/verify-email", authController.verifyEmail);
-router.post("/resend-verification", authController.resendVerificationEmail);
-router.post("/reset-password", authController.resetPassword);
+router.post(
+  "/login",
+  authMiddleware.validateLogin,
+  (req, res) => authController.loginUser(req, res),
+);
+router.post("/forgot-password", (req, res) =>
+  authController.forgotPassword(req, res),
+);
+router.post("/verify-email", (req, res) =>
+  authController.verifyEmail(req, res),
+);
+router.post("/resend-verification", (req, res) =>
+  authController.resendVerificationEmail(req, res),
+);
+router.post("/reset-password", (req, res) =>
+  authController.resetPassword(req, res),
+);
 
 module.exports = router;
